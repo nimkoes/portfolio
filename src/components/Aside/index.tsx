656ad585f1c30187e3af7a0ff97274eaa0501d5f
nimkoes <xxxelppa@gmail.com>
2025-03-04 07:41:00 +0900

    index.tsx 업데이트

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -19,7 +19,7 @@ const Aside = () => (
     </ul>
 
     <div className={styles.asideLink}>
-      <a href="https://xxxelppa.tistory.com" target="_blank" className={styles.asideLinkTistory}>
+      <a href="https://nimkoes.github.io/tech-blog/" target="_blank" className={styles.asideLinkTistory}>
         <svg viewBox="0 0 459 459" width={20} height={20} fill="currentColor" className={styles.asideLinkLogo}>
           <path d="M229.5,0C102.75,0,0,102.75,0,229.5S102.75,459,229.5,459,459,356.25,459,229.5,356.25,0,229.5,0ZM130.21,191.45a39.57,39.57,0,1,1,39.56-39.57A39.58,39.58,0,0,1,130.21,191.45ZM229.5,390a39.56,39.56,0,1,1,39.56-39.56A39.56,39.56,0,0,1,229.5,390Zm0-99.29a39.56,39.56,0,1,1,39.56-39.56A39.56,39.56,0,0,1,229.5,290.74Zm0-99.29a39.57,39.57,0,1,1,39.56-39.57A39.57,39.57,0,0,1,229.5,191.45Zm99.29,0a39.57,39.57,0,1,1,39.57-39.57A39.57,39.57,0,0,1,328.79,191.45Z" />
         </svg>
